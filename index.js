02054a7253d3f0d9e62c4d145ffca6a7ad9ff0ee
moazm-90 <moazomsaimgujjar@gmail.com>
2026-04-10 19:33:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express');
 const app = express();
-
 app.get('/', (req, res) => res.send('CI/CD Ready!!!'));
-
 app.listen(3000, () => console.log('App running on port 3000'));
