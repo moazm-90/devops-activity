19b8f514c6af938d6b71ad6b925e0b02e1bd880c
moazm-90 <moazomsaimgujjar@gmail.com>
2026-04-10 20:16:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('CI/CD Ready Updated'));
+app.get('/', (req, res) => {
+  res.send('CI/CD Ready - Pipeline Working Successfully');
+});
 
-app.listen(3000, () => console.log('App running on port 3000'));
+app.listen(3000, () => {
+  console.log('App running on port 3000');
+});
